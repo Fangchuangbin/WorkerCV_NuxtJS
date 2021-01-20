fe2ab66581a0a77d3e222c9014629f1f14d2ed31
fangchuangbin <fangchuangbin@qq.com>
2021-01-20 18:19:21 +0800

    main

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,10 @@
 <template>
   <header>
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-      <a class="navbar-brand" href="/">极速简历 WorkerCV</a>
+      <a class="navbar-brand pt-md-1 pt-2" href="/">极速简历 WorkerCV</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
@@ -31,7 +30,10 @@
             <a class="nav-link mx-md-1" href="#">招聘</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-md-1" href="#">排行榜</a>
+            <a class="nav-link mx-md-1" href="#">工具<svg t="1611136560411" class="mb-1 ml-1 icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4785" width="16" height="16"><path d="M965.76 576l-74.56-78.08a107.2 107.2 0 0 1-19.84-105.6l40.96-99.84a40.96 40.96 0 0 0-39.68-60.8l-107.84-0.64a107.2 107.2 0 0 1-88.64-60.48l-41.6-99.52a40.96 40.96 0 0 0-71.04-14.72l-78.08 74.24a107.2 107.2 0 0 1-105.6 19.84L280 109.76a40.96 40.96 0 0 0-60.8 39.68L216.96 256a107.2 107.2 0 0 1-60.48 88.64l-99.52 41.6a40.96 40.96 0 0 0-14.72 71.04l74.56 78.08a107.52 107.52 0 0 1 19.84 105.6L96 741.76a40.96 40.96 0 0 0 39.68 60.8l107.84 2.56A107.2 107.2 0 0 1 331.84 864l41.6 99.52a40.96 40.96 0 0 0 71.04 14.72l78.08-74.56a107.2 107.2 0 0 1 105.6-19.84l99.84 40.96a40.96 40.96 0 0 0 60.8-39.68l2.56-107.84a107.2 107.2 0 0 1 60.48-88.64l99.52-41.6a40.96 40.96 0 0 0 14.4-71.04z m-309.44 128c-18.56 3.2-43.2 0-79.36-34.56-25.28-24.96-102.72-118.08-154.88-181.12l37.76 187.52a73.92 73.92 0 0 1-1.92 39.68 51.84 51.84 0 0 1-65.6 29.12A64 64 0 0 1 352 686.72l-50.56-288A59.52 59.52 0 0 1 311.68 352a64 64 0 0 1 43.52-21.76A55.36 55.36 0 0 1 416 352c26.24 29.76 52.16 59.84 77.76 89.92 32 36.8 61.12 73.92 91.52 110.72l-39.04-195.52a64 64 0 0 1 3.2-37.12 48.64 48.64 0 0 1 39.04-32 52.8 52.8 0 0 1 40.96 9.28 71.36 71.36 0 0 1 24.64 47.04L704 630.72A55.68 55.68 0 0 1 656.32 704z" fill="#28a745" p-id="4786"></path></svg></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-md-1" href="#">排行榜<svg t="1611136414571" class="mb-1 ml-1 icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2590" width="16" height="16"><path d="M536.2112 0c27.362133 49.8048 44.4416 116.8768 44.4416 191.125333 0 153.2928-71.7184 277.597867-160.247467 277.597867-68.992 0-127.291733-76.002133-149.870933-181.994667C190.372267 399.940267 122.856533 534.045867 122.856533 666.773333 122.856533 864.0768 297.098667 1024 511.9808 1024c214.920533 0 389.162667-159.9232 389.162667-357.226667C901.1392 308.3776 550.5216 11.968 536.2112 0L536.2112 0zM515.197867 938.615467c-131.0976 0-170.218667-101.508267-163.464533-142.446933 13.4656-81.582933 94.0288-94.894933 154.88-94.894933 144.8192 0 203.1232-150.754133 206.5664-189.8368C713.175467 511.4368 765.314133 938.615467 515.197867 938.615467L515.197867 938.615467z" p-id="2591" fill="#dc3545"></path></svg></a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link mx-md-1 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -47,7 +49,7 @@
         </ul>
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="请输入关键字..." aria-label="请输入关键字...">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">搜索</button>
+          <button class="btn btn-outline-light my-2 my-sm-0" type="submit">搜索</button>
         </form>
       </div>
     </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,26 @@
 <template>
   <div id="__index">
     <Header />
-    <section class="container mt-4">
+    <section class="bg-light py-5 overflow-hidden position-relative">
+      <div class="container text-center py-5 index-section-1">
+        <h1 class="text-dark">极速简历 WorkerCV</h1>
+        <p class="lead py-2">海量模板任意挑选，一站式智能简历工具！</p>
+        <a class="btn btn-lg btn-dark mr-2 px-4" href="">开始制作</a>
+        <a class="btn btn-lg btn-outline-dark px-4" href="">选择模板</a>
+        <p class="bg-icon-left d-none d-md-block"><svg xmlns="http://www.w3.org/2000/svg" width="450" height="450" fill="currentColor" class="bi bi-file-earmark-ppt-fill" viewBox="0 0 16 16">
+          <path d="M6.5 8.5a2 2 0 1 0 4 0 2 2 0 0 0-4 0z"/>
+          <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.5 6v.264a3 3 0 1 1 0 4.472V13.5a.5.5 0 0 1-1 0V6a.5.5 0 0 1 1 0z"/>
+        </svg></p>
+        <p class="bg-icon-right d-none d-md-block"><svg xmlns="http://www.w3.org/2000/svg" width="450" height="450" fill="currentColor" class="bi bi-file-earmark-ppt-fill" viewBox="0 0 16 16">
+          <path d="M6.5 8.5a2 2 0 1 0 4 0 2 2 0 0 0-4 0z"/>
+          <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.5 6v.264a3 3 0 1 1 0 4.472V13.5a.5.5 0 0 1-1 0V6a.5.5 0 0 1 1 0z"/>
+        </svg></p>
+      </div>
+    </section>
+    <section class="container overflow-hidden">
       <div class="row">
-        <div class="col-md-9">
-          <div class="jumbotron bg-light border p-3 overflow-hidden">
-            <h2 class="font-size-30"><font class="text-danger">免费</font>、<font class="text-success">快捷</font>、<font class="text-primary">专业</font>的智能简历工具</h2>
-            <h4>极速简历 WorkerCV 拥有 <font class="text-primary">海量模板</font> 任意挑选，支持在线制作简历。</h4>
-            <p class="lead">极速简历 WorkerCV 提供各行业求职简历模板 <font class="text-primary">免费下载</font>，还有求职面试题库和企业招聘信息，以及大量简历制作技巧和求职攻略！</p>
-            <a class="btn btn-danger mr-1 mb-2" href="#" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-1 mb-1 bi bi-calendar3" viewBox="0 0 16 16">
-              <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
-              <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-            </svg>模板中心</a>
-            <a class="btn btn-success mr-1 mb-2" href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-1 mb-1 bi bi-archive-fill" viewBox="0 0 16 16">
-              <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
-            </svg>面试题库</a>
-            <a class="btn btn-primary mb-2" href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-1 mb-1 bi bi-disc-fill" viewBox="0 0 16 16">
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-6 0a2 2 0 1 0-4 0 2 2 0 0 0 4 0zM4 8a4 4 0 0 1 4-4 .5.5 0 0 0 0-1 5 5 0 0 0-5 5 .5.5 0 0 0 1 0zm9 0a.5.5 0 1 0-1 0 4 4 0 0 1-4 4 .5.5 0 0 0 0 1 5 5 0 0 0 5-5z"/>
-            </svg>招聘广场</a>
-            <small class="text-muted font-size-16 ml-3">注册成为会员，体验更多有趣的功能！</small>
-          </div>
-        </div>
-        <div class="col-md-3 pl-md-0">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Cras justo odio</li>
-              <li class="list-group-item">Dapibus ac facilisis in</li>
-              <li class="list-group-item">Vestibulum at eros</li>
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
+        <div class="col-md-12">
+
         </div>
       </div>
     </section>
