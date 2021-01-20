a8fc43b16d7b3a691c757083081ca846b2c1f062
fangchuangbin <fangchuangbin@qq.com>
2021-01-20 23:45:40 +0800

    main

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,24 @@
 <template>
   <div id="__index">
     <Header />
-    <section class="bg-light py-5 overflow-hidden position-relative">
-      <div class="container text-center py-5 index-section-1">
-        <h1 class="text-dark">极速简历 WorkerCV</h1>
-        <p class="lead py-2">海量模板任意挑选，一站式智能简历工具！</p>
-        <a class="btn btn-lg btn-dark mr-2 px-4" href="">开始制作</a>
-        <a class="btn btn-lg btn-outline-dark px-4" href="">选择模板</a>
-        <p class="bg-icon-left d-none d-md-block"><svg xmlns="http://www.w3.org/2000/svg" width="450" height="450" fill="currentColor" class="bi bi-file-earmark-ppt-fill" viewBox="0 0 16 16">
-          <path d="M6.5 8.5a2 2 0 1 0 4 0 2 2 0 0 0-4 0z"/>
-          <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.5 6v.264a3 3 0 1 1 0 4.472V13.5a.5.5 0 0 1-1 0V6a.5.5 0 0 1 1 0z"/>
+    <section class="bg-white py-4 overflow-hidden position-relative shadow-sm">
+      <div class="container text-center pt-2 pb-3 index-section-1">
+        <h2 class="text-dark">极速简历 WorkerCV</h2>
+        <p class="lead py-1">海量模板任意挑选，一站式智能简历工具！</p>
+        <a class="btn btn-dark mx-1 px-5 mb-md-0 mb-2" href="">开始制作</a>
+        <a class="btn btn-outline-dark mx-1 px-5" href="">加入会员</a>
+        <p class="bg-icon-left d-none d-md-block m-0"><svg xmlns="http://www.w3.org/2000/svg" width="350" height="350" fill="currentColor" class="bi bi-file-word-fill" viewBox="0 0 16 16">
+          <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5.485 4.879l1.036 4.144.997-3.655a.5.5 0 0 1 .964 0l.997 3.655 1.036-4.144a.5.5 0 0 1 .97.242l-1.5 6a.5.5 0 0 1-.967.01L8 7.402l-1.018 3.73a.5.5 0 0 1-.967-.01l-1.5-6a.5.5 0 1 1 .97-.242z"/>
         </svg></p>
-        <p class="bg-icon-right d-none d-md-block"><svg xmlns="http://www.w3.org/2000/svg" width="450" height="450" fill="currentColor" class="bi bi-file-earmark-ppt-fill" viewBox="0 0 16 16">
-          <path d="M6.5 8.5a2 2 0 1 0 4 0 2 2 0 0 0-4 0z"/>
-          <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.5 6v.264a3 3 0 1 1 0 4.472V13.5a.5.5 0 0 1-1 0V6a.5.5 0 0 1 1 0z"/>
+        <p class="bg-icon-right d-none d-md-block m-0"><svg xmlns="http://www.w3.org/2000/svg" width="350" height="350" fill="currentColor" class="bi bi-file-word-fill" viewBox="0 0 16 16">
+          <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5.485 4.879l1.036 4.144.997-3.655a.5.5 0 0 1 .964 0l.997 3.655 1.036-4.144a.5.5 0 0 1 .97.242l-1.5 6a.5.5 0 0 1-.967.01L8 7.402l-1.018 3.73a.5.5 0 0 1-.967-.01l-1.5-6a.5.5 0 1 1 .97-.242z"/>
         </svg></p>
       </div>
     </section>
     <section class="container overflow-hidden">
       <div class="row">
-        <div class="col-md-12">
-
+        <div class="col-md-12 py-5">
+          13
         </div>
       </div>
     </section>
