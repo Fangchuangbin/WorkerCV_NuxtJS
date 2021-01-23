335d16af99ff72b25e12020fdf907f4efcb2c405
fangchuangbin <fangchuangbin@qq.com>
2021-01-23 13:19:49 +0800

    main

--- pages/template/_id.vue
+++ pages/template/_id.vue
@@ -1,10 +1,131 @@
 <template>
   <div id="__template">
     <Header />
-    <section class="template-section-3 bg-white shadow-sm py-4 overflow-hidden">
+    <section class="template-section-3 py-4 overflow-hidden">
       <div class="container">
-        <h3 class="text-dark">Java程序员简历模板</h3>
-        <p class="text-muted mt-2 font-size-14">发布于2020-12-19 / 已有12321人使用</p>
+        <div class="row">
+          <div class="col-md-12 mb-2">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb bg-white shadow-sm font-size-14">
+                <li class="breadcrumb-item"><a class="text-dark" href="#">首页</a></li>
+                <li class="breadcrumb-item"><a class="text-dark" href="#">秋招简历</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Java程序员简历模板</li>
+              </ol>
+            </nav>
+          </div>
+          <div class="col-md-2">
+            <div class="nav flex-column nav-pills">
+              <a class="nav-link active bg-dark" href="#v-pills-home"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mb-1 mr-2 bi bi-file-earmark-word-fill" viewBox="0 0 16 16">
+                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM5.485 6.879l1.036 4.144.997-3.655a.5.5 0 0 1 .964 0l.997 3.655 1.036-4.144a.5.5 0 0 1 .97.242l-1.5 6a.5.5 0 0 1-.967.01L8 9.402l-1.018 3.73a.5.5 0 0 1-.967-.01l-1.5-6a.5.5 0 1 1 .97-.242z"/>
+              </svg>模板详情</a>
+              <a class="nav-link text-dark" href="#v-pills-profile"><svg t="1611136414571" class="mb-1 mr-2 icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2590" width="16" height="16"><path d="M536.2112 0c27.362133 49.8048 44.4416 116.8768 44.4416 191.125333 0 153.2928-71.7184 277.597867-160.247467 277.597867-68.992 0-127.291733-76.002133-149.870933-181.994667C190.372267 399.940267 122.856533 534.045867 122.856533 666.773333 122.856533 864.0768 297.098667 1024 511.9808 1024c214.920533 0 389.162667-159.9232 389.162667-357.226667C901.1392 308.3776 550.5216 11.968 536.2112 0L536.2112 0zM515.197867 938.615467c-131.0976 0-170.218667-101.508267-163.464533-142.446933 13.4656-81.582933 94.0288-94.894933 154.88-94.894933 144.8192 0 203.1232-150.754133 206.5664-189.8368C713.175467 511.4368 765.314133 938.615467 515.197867 938.615467L515.197867 938.615467z" p-id="2591"></path></svg>热门职位</a>
+              <a class="nav-link text-dark" href="#v-pills-messages"><svg t="1611136560411" class="mb-1 mr-2 icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4785" width="16" height="16"><path d="M965.76 576l-74.56-78.08a107.2 107.2 0 0 1-19.84-105.6l40.96-99.84a40.96 40.96 0 0 0-39.68-60.8l-107.84-0.64a107.2 107.2 0 0 1-88.64-60.48l-41.6-99.52a40.96 40.96 0 0 0-71.04-14.72l-78.08 74.24a107.2 107.2 0 0 1-105.6 19.84L280 109.76a40.96 40.96 0 0 0-60.8 39.68L216.96 256a107.2 107.2 0 0 1-60.48 88.64l-99.52 41.6a40.96 40.96 0 0 0-14.72 71.04l74.56 78.08a107.52 107.52 0 0 1 19.84 105.6L96 741.76a40.96 40.96 0 0 0 39.68 60.8l107.84 2.56A107.2 107.2 0 0 1 331.84 864l41.6 99.52a40.96 40.96 0 0 0 71.04 14.72l78.08-74.56a107.2 107.2 0 0 1 105.6-19.84l99.84 40.96a40.96 40.96 0 0 0 60.8-39.68l2.56-107.84a107.2 107.2 0 0 1 60.48-88.64l99.52-41.6a40.96 40.96 0 0 0 14.4-71.04z m-309.44 128c-18.56 3.2-43.2 0-79.36-34.56-25.28-24.96-102.72-118.08-154.88-181.12l37.76 187.52a73.92 73.92 0 0 1-1.92 39.68 51.84 51.84 0 0 1-65.6 29.12A64 64 0 0 1 352 686.72l-50.56-288A59.52 59.52 0 0 1 311.68 352a64 64 0 0 1 43.52-21.76A55.36 55.36 0 0 1 416 352c26.24 29.76 52.16 59.84 77.76 89.92 32 36.8 61.12 73.92 91.52 110.72l-39.04-195.52a64 64 0 0 1 3.2-37.12 48.64 48.64 0 0 1 39.04-32 52.8 52.8 0 0 1 40.96 9.28 71.36 71.36 0 0 1 24.64 47.04L704 630.72A55.68 55.68 0 0 1 656.32 704z" p-id="4786"></path></svg>最新工具</a>
+            </div>
+            <div class="dropdown-divider"></div>
+            <h6 class="py-2 mb-1">热门标签</h6>
+            <div class="template-cv mb-4 mb-md-0">
+              <a href="#" class="badge badge-primary">实习生</a>
+              <a href="#" class="badge badge-secondary">应届生</a>
+              <a href="#" class="badge badge-success">大学生</a>
+              <a href="#" class="badge badge-danger">留学生</a>
+              <a href="#" class="badge badge-secondary">校招</a>
+              <a href="#" class="badge badge-info">社招</a>
+              <a href="#" class="badge badge-warning">英文</a>
+              <p class="py-1"></p>
+              <a href="#" class="badge badge-info">互联网</a>
+              <a href="#" class="badge badge-secondary">金融</a>
+              <a href="#" class="badge badge-success">咨询</a>
+              <a href="#" class="badge badge-danger">银行</a>
+              <a href="#" class="badge badge-secondary">文化传媒</a>
+              <a href="#" class="badge badge-info">房地产</a>
+              <a href="#" class="badge badge-success">通信</a>
+              <a href="#" class="badge badge-secondary">游戏</a>
+              <a href="#" class="badge badge-success">制造业</a>
+              <a href="#" class="badge badge-danger">汽车</a>
+              <a href="#" class="badge badge-secondary">物流</a>
+              <a href="#" class="badge badge-info">教育培训</a>
+              <p class="py-1"></p>
+              <a href="#" class="badge badge-success">产品经理</a>
+              <a href="#" class="badge badge-secondary">程序员</a>
+              <a href="#" class="badge badge-success">运营</a>
+              <a href="#" class="badge badge-danger">行政</a>
+              <a href="#" class="badge badge-danger">设计</a>
+              <a href="#" class="badge badge-info">财务</a>
+              <a href="#" class="badge badge-secondary">Web前端</a>
+              <a href="#" class="badge badge-primary">Java</a>
+              <a href="#" class="badge badge-warning">Android</a>
+              <a href="#" class="badge badge-danger">IOS</a>
+              <a href="#" class="badge badge-success">测试</a>
+              <a href="#" class="badge badge-info">运维</a>
+              <a href="#" class="badge badge-success">平面设计</a>
+              <a href="#" class="badge badge-secondary">人力资源</a>
+              <a href="#" class="badge badge-success">会展策划</a>
+              <a href="#" class="badge badge-danger">医疗健康</a>
+              <a href="#" class="badge badge-warning">品牌公关</a>
+              <a href="#" class="badge badge-info">Javascript</a>
+              <a href="#" class="badge badge-success">网络安全</a>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 mb-md-0">
+            <img class="rounded-lg shadow-sm" width="100%" src="https://prod-file-uploaded.oss-cn-beijing.aliyuncs.com/template_resumes/c6d424f0-d7d1-11ea-a54c-47d0975e9829.png" alt="">
+          </div>
+          <div class="col-md-4">
+            <div class="bg-white shadow-sm overflow-hidden rounded-lg p-4">
+              <h4 class="text-dark">Java程序员简历模板</h4>
+              <p class="text-muted mt-2 font-size-14">发布于2020-12-19 / 已有12321人使用</p>
+              <p class="bg-light rounded-lg font-size-14 py-2 px-3 border mt-3 mb-4 text-muted">适合2021届秋招投递参考，专为应届生秋招设计的简历模板，有实习、项目、校园经历，帮你在秋招中脱颖而出 | 2021秋招简历模板</p>
+              <a class="btn btn-danger px-4 mr-1" href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2 bi bi-hand-index-thumb-fill" viewBox="0 0 16 16">
+                <path d="M8.5 1.75v2.716l.048-.002c.311-.012.74-.016 1.05.046.28.056.543.18.738.288.274.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716.075.09.141.175.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0z"/>
+              </svg>使用模板</a>
+              <a class="btn btn-outline-dark px-4" href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-2 bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+              </svg>收藏</a>
+            </div>
+            <div class="mt-4 bg-white shadow-sm overflow-hidden rounded-lg p-4">
+              <h6><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mb-1 mr-2 bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
+                <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.964.22.817.533 2.512.062 4.51a9.84 9.84 0 0 1 .443-.05c.713-.065 1.669-.072 2.516.21.518.173.994.68 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.162 3.162 0 0 1-.488.9c.054.153.076.313.076.465 0 .306-.089.626-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.826 4.826 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.616.849-.231 1.574-.786 2.132-1.41.56-.626.914-1.279 1.039-1.638.199-.575.356-1.54.428-2.59z"/>
+              </svg>推荐模板</h6>
+              <ul class="row recommend-editer">
+                <li class="col-12 col-md-6 overflow-hidden mt-3 pb-2">
+                  <div class="card bg-white rounded-lg"><a href="">
+                    <img src="https://prod-file-uploaded.oss-cn-beijing.aliyuncs.com/template_resumes/c6d424f0-d7d1-11ea-a54c-47d0975e9829.png?imageMogr2/auto-orient/thumbnail/560x792/interlace/1/blur/1x0/quality/100" class="card-img-top" alt="...">
+                    <div class="card-body px-3 py-2 border-top">
+                      <p class="card-text text-dark title font-size-14">程序员简历模板</p>
+                      <p class="card-text text-dark"><span class="badge badge-success mr-2">免费</span><span class="badge badge-dark">程序员</span></p>
+                    </div>
+                  </a></div>
+                </li>
+                <li class="col-12 col-md-6 overflow-hidden mt-3 pb-2">
+                  <div class="card bg-light rounded-lg"><a href="">
+                    <img src="https://prod-file-uploaded.oss-cn-beijing.aliyuncs.com/template_resumes/c6d424f0-d7d1-11ea-a54c-47d0975e9829.png?imageMogr2/auto-orient/thumbnail/560x792/interlace/1/blur/1x0/quality/100" class="card-img-top" alt="...">
+                    <div class="card-body px-3 py-2 border-top">
+                      <p class="card-text text-dark title font-size-14">财务简历模板</p>
+                      <p class="card-text text-dark"><span class="badge badge-warning mr-2">会员</span><span class="badge badge-dark">财务管理</span></p>
+                    </div>
+                  </a></div>
+                </li>
+                <li class="col-12 col-md-6 overflow-hidden mt-3 pb-2">
+                  <div class="card bg-light rounded-lg"><a href="">
+                    <img src="https://prod-file-uploaded.oss-cn-beijing.aliyuncs.com/template_resumes/c6d424f0-d7d1-11ea-a54c-47d0975e9829.png?imageMogr2/auto-orient/thumbnail/560x792/interlace/1/blur/1x0/quality/100" class="card-img-top" alt="...">
+                    <div class="card-body px-3 py-2 border-top">
+                      <p class="card-text text-dark title font-size-14">Java开发简历模板</p>
+                      <p class="card-text text-dark"><span class="badge badge-warning mr-2">会员</span><span class="badge badge-dark">Java</span></p>
+                    </div>
+                  </a></div>
+                </li>
+                <li class="col-12 col-md-6 overflow-hidden mt-3 pb-2">
+                  <div class="card bg-light rounded-lg"><a href="">
+                    <img src="https://prod-file-uploaded.oss-cn-beijing.aliyuncs.com/template_resumes/c6d424f0-d7d1-11ea-a54c-47d0975e9829.png?imageMogr2/auto-orient/thumbnail/560x792/interlace/1/blur/1x0/quality/100" class="card-img-top" alt="...">
+                    <div class="card-body px-3 py-2 border-top">
+                      <p class="card-text text-dark title font-size-14">Android开发简历模板</p>
+                      <p class="card-text text-dark"><span class="badge badge-warning mr-2">会员</span><span class="badge badge-dark">Android</span></p>
+                    </div>
+                  </a></div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
     <Footer />
